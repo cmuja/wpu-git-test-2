2ed424ff9fc113b5d9371b4ee6681717fb7a989c
cmuja <cmujaidi@gmail.com>
2022-08-27 00:24:22 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, voluptates vitae! Voluptatibus quod nostrum, quisquam accusantium optio temporibus praesentium earum iure eveniet obcaecati voluptatem, quae sint doloribus aliquam doloremque quo.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
     
 </body>
 </html>
